07d31d026653144a6d4941b3e7e9bd8a0a355fe0
srushtiannigeri <149875053+srushtiannigeri@users.noreply.github.com>
2025-03-10 16:18:17 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, PES2UG22CS581\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
